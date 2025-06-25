6bde70345bf9e15fce67b637afd8468f41cf311c
hnarayanan11 <hnarayanan11@gmail.com>
2025-06-25 12:02:11 +0530

    Initial Commit

---
+++ src/main/java/com/example/e_com_product/EComProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_com_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComProductApplication.class, args);
+	}
+
+}
